b2e0d620c1f6b7b3cc2713df180bf8394b8e1b50
Erick0105 <erickaxs0105@gmail.com>
2025-04-02 15:29:11 -0300

    Cap 1 completo

---
+++ criandoPrimeiraApp/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,15 @@
+﻿// Screen Sound
+
+//C# Linguagem fortemente Tipada
+
+//Criando uma var
+string mensagemBoasVindas = "Boas Vindas ao Screen Sound";
+Console.WriteLine(mensagemBoasVindas);
+
+//Praticando
+
+string curso = "C#: Criando sua primeira aplicação";
+string nome = "Erick";
+string sobrenome = "Alves";
+
+Console.WriteLine("Estudando: " + curso + "\nAluno: " + nome + "\nSobrenome: " + sobrenome + "\n Com os intrutores Daniel Portugal e Gui Lima");
